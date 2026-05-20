85c219c58aa73802602b444103518097a1af37b5
ST10506115 <ST10506115@myemeris.edu.za>
2026-05-20 15:55:19 +0200

    Update Program.java

--- Program.java
+++ Program.java
@@ -9,7 +9,8 @@ package program;
  * @author Nathan Cant
  */
 import java.util.Scanner;
-public class Program {
+public class Program 
+{
 
     /**
      * @param args the command line arguments
@@ -46,7 +47,8 @@ public class Program {
         System.out.println();
         System.out.println(registrationResult);
  
-        if (!registrationResult.contains("successfully")) {
+        if (!registrationResult.contains("successfully")) 
+        {
             System.out.println("\nRegistration failed. Please restart and try again.");
             scanner.close();
             return;
@@ -64,12 +66,118 @@ public class Program {
         String loginPassword = scanner.nextLine();
  
         Login login = new Login(loginUsername, loginPassword, newUser);
+        String loginStatus = login.returnLoginStatus();
         System.out.println();
         System.out.println(login.returnLoginStatus());
+        
+        if (!login.loginUser()) 
+        {
+            System.out.println("Exiting. Please restart and try again.");
+            scanner.close();
+            return;
+        }
+
+          System.out.println();        
+        
+//Messaging
+        System.out.println("Welcome to QuickChat.");
+        System.out.println();
  
-        scanner.close();
+// how many messages does the user want to send
+        System.out.print("How many messages would you like to send? ");
+        int numMessages = Integer.parseInt(scanner.nextLine());
+ 
+        Message lastMessage = null;
+        int menuChoice = 0;
+ 
+// Keep running until user quits
+        do 
+        {
+            System.out.println();
+            System.out.println("   Menu   ");
+            System.out.println("1) Send Messages");
+            System.out.println("2) Show recently sent messages");
+            System.out.println("3) Quit");
+            System.out.print("Choose an option: ");
+            menuChoice = Integer.parseInt(scanner.nextLine());
+ 
+            switch (menuChoice) 
+            {
+                case 1:
+                    // Send messages up to the number the user defined
+                    for (int i = 1; i <= numMessages; i++) 
+                    {
+                        System.out.println("\nMessage " + i + " of " + numMessages + " ");
+ 
+                        System.out.print("Enter recipient number (with international code): ");
+                        String recipient = scanner.nextLine();
+ 
+                        System.out.print("Enter message (max 250 characters): ");
+                        String messageText = scanner.nextLine();
+ 
+                        Message msg = new Message(recipient, messageText, i);
+                        lastMessage = msg;
+ 
+// Check message length first
+String lengthCheck = msg.checkMessageLength();
+    if (!lengthCheck.equals("Message ready to send.")) 
+    {
+        System.out.println(lengthCheck);
+        i--; // let user retry this message
+        continue;
     }
+ 
+// Check recipient
+    System.out.println(msg.checkRecipientCell());
+ 
+// Show message details
+    System.out.println("Message ID: "   + msg.getMessageID());
+    System.out.println("Message Hash: " + msg.getMessageHash());
+ 
+// Ask what to do with the message
+    System.out.println("\nWhat would you like to do?");
+    System.out.println("1) Send Message");
+    System.out.println("2) Disregard Message");
+    System.out.println("3) Store Message to send later");
+    System.out.print("Choose: ");
+        int sendChoice = Integer.parseInt(scanner.nextLine());
+ 
+    System.out.println(msg.sentMessage(sendChoice));
+                    }
+ 
+// Display all message details after sending
+    if (lastMessage != null) 
+    {
+        System.out.println("\n--- All Sent Messages ---");
+        System.out.println(lastMessage.printMessages());
+        System.out.println("Total messages sent: " + lastMessage.returnTotalMessages());
     }
+        break;
+ 
+        case 2:
+            
+            System.out.println("Coming Soon.");
+            break;
+ 
+        case 3:
+                    
+            System.out.println("Goodbye!");
+            break;
+ 
+        default:
+            System.out.println("Invalid option. Please try again.");
+            }
+ 
+        } 
+        while (menuChoice != 3);
+ 
+        scanner.close();      
+    }
+}
+    
+
+
+
     
 
 /*REFRENCES: 
